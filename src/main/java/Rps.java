60830f47e6fbfbb48e099ca955ca60ec2457aac9
Mick Sexton and Saul Artiga <mick@>
2017-06-28 11:21:14 -0700

    ADD ALL TESTS AND METHODS FOR TESTING VICTORY

--- src/main/java/Rps.java
+++ src/main/java/Rps.java
@@ -1,11 +1,34 @@
 public class Rps {
-
+  public String hand;
   public boolean validateUserInput(String input) {
     boolean validate;
     if (input.equals("rock") || input.equals("scissors") || input.equals("paper") ) {
       validate = true;
-    } else { validate = false; System.out.println("Not a valid selection.");}
+      hand = input;
+    } else {
+     validate = false;
+     System.out.println("Not a valid selection.");
+   }
     return validate;
+  }
 
+  public String calculateWinner(String p1Hand, String p2Hand){
+    if (p1Hand.equals(p2Hand)){
+      return "Tie!";
+    } else if (p1Hand.equals("rock") && p2Hand.equals("scissors")) {
+      return "Player 1 Wins!";
+    } else if (p2Hand.equals("rock") && p1Hand.equals("scissors")) {
+      return "Player 2 Wins!";
+    } else if (p1Hand.equals("paper") && p2Hand.equals("rock")) {
+      return "Player 1 Wins!";
+    } else if (p2Hand.equals("paper") && p1Hand.equals("rock")) {
+      return "Player 2 Wins!";
+    } else if (p1Hand.equals("scissors") && p2Hand.equals("paper")) {
+      return "Player 1 Wins!";
+    } else if (p2Hand.equals("scissors") && p1Hand.equals("paper")) {
+      return "Player 2 Wins!";
+    } else {
+      return "??";
+    }
   }
 }
--- src/test/java/RpsTest.java
+++ src/test/java/RpsTest.java
@@ -4,10 +4,75 @@ import static org.junit.Assert.*;
 public class RpsTest {
 
   @Test
-  public void validateUserInput_TestsForRps_True(){
+  public void validateUserInput_TestsForPlayerOneInput_True(){
     Rps playerOne = new Rps();
     boolean expectedOutput = true;
     assertEquals(expectedOutput, playerOne.validateUserInput("rock"));
+  }
+
+  @Test
+  public void validateUserInput_TestsForPlayerTwoInput_True(){
+    Rps playerTwo = new Rps();
+    boolean expectedOutput = true;
+    assertEquals(expectedOutput, playerTwo.validateUserInput("paper"));
+  }
+
+  @Test
+  public void calculateWinner_TestsForPlayersTie_Tie(){
+    Rps playerOne = new Rps();
+    Rps playerTwo = new Rps();
+    String expectedOutput = "Tie!";
+    //String[] playerHands = {playerOne.hand, playerTwo.hand};
+    assertEquals(expectedOutput, playerOne.calculateWinner("rock", "rock"));
+  }
+
+  @Test
+  public void calculateWinner_TestForPlayersRockVScissors_Player_1_wins() {
+     Rps playerOne = new Rps();
+     Rps playerTwo = new Rps();
+     String expectedOutput = "Player 1 Wins!";
+     assertEquals(expectedOutput, playerOne.calculateWinner("rock", "scissors"));
+  }
+
+  @Test
+  public void calculateWinner_TestForPlayersPaperVSROCK_Player_1_wins() {
+     Rps playerOne = new Rps();
+     Rps playerTwo = new Rps();
+     String expectedOutput = "Player 1 Wins!";
+     assertEquals(expectedOutput, playerOne.calculateWinner("paper", "rock"));
+  }
+
+  @Test
+  public void calculateWinner_TestForPlayersScissorsVSPaper_Player_1_wins() {
+     Rps playerOne = new Rps();
+     Rps playerTwo = new Rps();
+     String expectedOutput = "Player 1 Wins!";
+     assertEquals(expectedOutput, playerOne.calculateWinner("scissors", "paper"));
+  }
+
+  @Test
+  public void calculateWinner_TestForPlayersPaperVSROCK_Player_2_wins() {
+     Rps playerOne = new Rps();
+     Rps playerTwo = new Rps();
+     String expectedOutput = "Player 2 Wins!";
+     assertEquals(expectedOutput, playerOne.calculateWinner("rock", "paper"));
+  }
+
+
+  @Test
+  public void calculateWinner_TestForPlayersRockVScissors_Player_2_wins() {
+     Rps playerOne = new Rps();
+     Rps playerTwo = new Rps();
+     String expectedOutput = "Player 2 Wins!";
+     assertEquals(expectedOutput, playerOne.calculateWinner("scissors", "rock"));
+  }
+
+  @Test
+  public void calculateWinner_TestForPlayersScissorsVSPaper_Player_2_wins() {
+     Rps playerOne = new Rps();
+     Rps playerTwo = new Rps();
+     String expectedOutput = "Player 2 Wins!";
+     assertEquals(expectedOutput, playerOne.calculateWinner("paper", "scissors"));
   }
 
 }
